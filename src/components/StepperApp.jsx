e4da8b58bb1cc4c19778cdba42c169e1edf517fd
itsMohammedNayeem <nayeem.gmit@gmail.com>
2024-02-13 20:51:05 +0530

    RoadSideCoder 2nd update

--- src/components/StepperApp.jsx
+++ src/components/StepperApp.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import cx from "classnames";
 
 const StepperApp = ({ stepsConfig = [] }) => {
-  const [currentStep, setCurrentStep] = useState(1);
+  console.log("🚀 ~ StepperApp ~ stepsConfig:", stepsConfig);
+  const [currentStep, setCurrentStep] = useState(2);
   const [isComplete, setIsComplete] = useState(false);
 
   if (stepsConfig.length === 0) {
@@ -14,23 +16,43 @@ const StepperApp = ({ stepsConfig = [] }) => {
 
   const handleNext = () => {};
 
+  const ActiveComponent = stepsConfig[currentStep - 1]?.Component;
+
   return (
     <>
       <div className="stepper p-4 relative flex items-center mb-[20px] justify-between">
         {stepsConfig.map((step, index) => {
           return (
             <div
-              className="step flex flex-col relative items-center"
+              className={cx("step flex flex-col relative items-center")}
               key={step.name}
             >
-              <div className="step-number size-[30px] rounded-full bg-[#ccc] mb-[5px] flex items-center justify-center z-2">
-                {index + 1}
+              <div
+                className={cx(
+                  "step-number size-[30px] rounded-full bg-[#ccc] mb-[5px] flex items-center justify-center z-2",
+                  {
+                    "bg-[#007bff] text-[#fff]": currentStep === index + 1,
+                    "bg-[#28a745] text-[#fff]":
+                      currentStep > index + 1 || isComplete,
+                  }
+                )}
+              >
+                {currentStep > index + 1 || isComplete ? (
+                  <span>&#10003;</span>
+                ) : (
+                  index + 1
+                )}
               </div>
               <div className="step-name text-[14px]">{step.name}</div>
             </div>
           );
         })}
       </div>
+
+      <div className="step-content text-center">
+        <ActiveComponent />
+      </div>
+
       {!isComplete && (
         <button className="rounded-md bg-blue-500 p-2" onClick={handleNext}>
           {currentStep === stepsConfig.length ? "Finish" : "Next"}
--- src/lib/api.jsx
+++ src/lib/api.jsx
@@ -1,9 +1,9 @@
 export const CHECKOUT_STEPS = [
   { name: "Ordered", Component: () => <p>Your order is placed</p> },
-  { name: "Shipped", status: () => <p>Your order is shipped</p> },
+  { name: "Shipped", Component: () => <p>Your order is shipped</p> },
   {
     name: "Out for delivery",
-    status: () => <p>Your order is out for delivery</p>,
+    Component: () => <p>Your order is out for delivery</p>,
   },
-  { name: "Delivered", status: () => <p>Your order has been delivered</p> },
+  { name: "Delivered", Component: () => <p>Your order has been delivered</p> },
 ];
